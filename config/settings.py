ae0166c23b37a7c7ed0de907f7c8f72bb489b642
Colin Shevlin <cwshevlin@gmail.com>
2020-12-24 12:56:47 -0600

    Moved the settings to the bottom.

--- config/settings.py
+++ config/settings.py
@@ -10,8 +10,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 import django_heroku
 
-# Activate Django-Heroku.
-django_heroku.settings(locals())
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -153,3 +151,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = 'static/'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
