e184b30765f04bf852d99f32ac3cbaa8498e83de
Colin Shevlin <cwshevlin@gmail.com>
2020-12-27 16:11:23 -0600

    Adds staticdirs.

--- config/settings.py
+++ config/settings.py
@@ -151,5 +151,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_DIRS = (
+    os.path.join(os.path.dirname(__file__), 'templates'),
+)
 
 django_heroku.settings(locals())
